7db36bf64e45c335da6a4388977bc8028f867673
stephanpeck <62480543+stephanpeck@users.noreply.github.com>
2021-04-09 15:18:04 -0600

    Scaffolded carbon and samples

---
+++ Controllers/Carbon2Controller.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fag_el_Gamous.Models;
+
+namespace Fag_el_Gamous.Controllers
+{
+    public class Carbon2Controller : Controller
+    {
+        private readonly waterbuffaloContext _context;
+
+        public Carbon2Controller(waterbuffaloContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carbon2
+        public async Task<IActionResult> Index()
+        {
+            var waterbuffaloContext = _context.Carbon2.Include(c => c.Burial);
+            return View(await waterbuffaloContext.ToListAsync());
+        }
+
+        // GET: Carbon2/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carbon2 = await _context.Carbon2
+                .Include(c => c.Burial)
+                .FirstOrDefaultAsync(m => m.CarbonId == id);
+            if (carbon2 == null)
+            {
+                return NotFound();
+            }
+
+            return View(carbon2);
+        }
+
+        // GET: Carbon2/Create
+        public IActionResult Create()
+        {
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId");
+            return View();
+        }
+
+        // POST: Carbon2/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CarbonId,BurialId,LocConcat,RackNum,LocationNs,Ns,LocationEw,Ew,BurialSubplot,Area,BurialNum,RackAlt,TubeNum,Description,SizeMl,Foci,C14Sample2017,Location,Questions,Num,Conventional14cAgeBp,CCalendarDate,Calibrated95PercentCalendarDateMax,Calibrated95PercentCalendarDateMin,Calibrated95PercentCalendarDateSpan,Calibrated95PercentCalendarDateAvg,Category,Notes")] Carbon2 carbon2)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carbon2);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId", carbon2.BurialId);
+            return View(carbon2);
+        }
+
+        // GET: Carbon2/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carbon2 = await _context.Carbon2.FindAsync(id);
+            if (carbon2 == null)
+            {
+                return NotFound();
+            }
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId", carbon2.BurialId);
+            return View(carbon2);
+        }
+
+        // POST: Carbon2/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CarbonId,BurialId,LocConcat,RackNum,LocationNs,Ns,LocationEw,Ew,BurialSubplot,Area,BurialNum,RackAlt,TubeNum,Description,SizeMl,Foci,C14Sample2017,Location,Questions,Num,Conventional14cAgeBp,CCalendarDate,Calibrated95PercentCalendarDateMax,Calibrated95PercentCalendarDateMin,Calibrated95PercentCalendarDateSpan,Calibrated95PercentCalendarDateAvg,Category,Notes")] Carbon2 carbon2)
+        {
+            if (id != carbon2.CarbonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carbon2);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Carbon2Exists(carbon2.CarbonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId", carbon2.BurialId);
+            return View(carbon2);
+        }
+
+        // GET: Carbon2/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carbon2 = await _context.Carbon2
+                .Include(c => c.Burial)
+                .FirstOrDefaultAsync(m => m.CarbonId == id);
+            if (carbon2 == null)
+            {
+                return NotFound();
+            }
+
+            return View(carbon2);
+        }
+
+        // POST: Carbon2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var carbon2 = await _context.Carbon2.FindAsync(id);
+            _context.Carbon2.Remove(carbon2);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool Carbon2Exists(int id)
+        {
+            return _context.Carbon2.Any(e => e.CarbonId == id);
+        }
+    }
+}
---
+++ Controllers/Samples2Controller.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fag_el_Gamous.Models;
+
+namespace Fag_el_Gamous.Controllers
+{
+    public class Samples2Controller : Controller
+    {
+        private readonly waterbuffaloContext _context;
+
+        public Samples2Controller(waterbuffaloContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Samples2
+        public async Task<IActionResult> Index()
+        {
+            var waterbuffaloContext = _context.Samples2.Include(s => s.Burial);
+            return View(await waterbuffaloContext.ToListAsync());
+        }
+
+        // GET: Samples2/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var samples2 = await _context.Samples2
+                .Include(s => s.Burial)
+                .FirstOrDefaultAsync(m => m.SampleId == id);
+            if (samples2 == null)
+            {
+                return NotFound();
+            }
+
+            return View(samples2);
+        }
+
+        // GET: Samples2/Create
+        public IActionResult Create()
+        {
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId");
+            return View();
+        }
+
+        // POST: Samples2/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SampleId,BurialId,LocConcat,RackNum,BagNum,LowNs,HighNs,NS,LowEw,HighEw,EW,Area,BurialNum,SubBurialNum,ClusterNum,DateDay,DateMonth,DateYear,PreviouslySampled,Notes,Initials")] Samples2 samples2)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(samples2);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId", samples2.BurialId);
+            return View(samples2);
+        }
+
+        // GET: Samples2/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var samples2 = await _context.Samples2.FindAsync(id);
+            if (samples2 == null)
+            {
+                return NotFound();
+            }
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId", samples2.BurialId);
+            return View(samples2);
+        }
+
+        // POST: Samples2/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SampleId,BurialId,LocConcat,RackNum,BagNum,LowNs,HighNs,NS,LowEw,HighEw,EW,Area,BurialNum,SubBurialNum,ClusterNum,DateDay,DateMonth,DateYear,PreviouslySampled,Notes,Initials")] Samples2 samples2)
+        {
+            if (id != samples2.SampleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(samples2);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Samples2Exists(samples2.SampleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BurialId"] = new SelectList(_context.MasterBurial2, "BurialId", "BurialId", samples2.BurialId);
+            return View(samples2);
+        }
+
+        // GET: Samples2/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var samples2 = await _context.Samples2
+                .Include(s => s.Burial)
+                .FirstOrDefaultAsync(m => m.SampleId == id);
+            if (samples2 == null)
+            {
+                return NotFound();
+            }
+
+            return View(samples2);
+        }
+
+        // POST: Samples2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var samples2 = await _context.Samples2.FindAsync(id);
+            _context.Samples2.Remove(samples2);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool Samples2Exists(int id)
+        {
+            return _context.Samples2.Any(e => e.SampleId == id);
+        }
+    }
+}
