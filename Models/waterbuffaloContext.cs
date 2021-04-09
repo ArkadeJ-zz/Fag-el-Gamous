70e67c2f1179c1d8e2f95d30676e06ce46ccb637
stephanpeck <62480543+stephanpeck@users.noreply.github.com>
2021-04-09 00:16:44 -0600

    db fix

--- Models/waterbuffaloContext.cs
+++ Models/waterbuffaloContext.cs
@@ -28,7 +28,7 @@ namespace Fag_el_Gamous.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql("Host = localhost; Database = waterbuffalo; Username=postgres; Password=sn0wb1rd");
+                optionsBuilder.UseNpgsql("Host = localhost; Database = waterbuffalo; Username=postgres; Password=profgreg");
             }
         }
 
