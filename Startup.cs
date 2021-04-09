d9523f5f81e69b9668ee64e5be3ba7fedf25f0ec
joshcard <66044735+joshcard@users.noreply.github.com>
2021-04-09 14:55:52 -0600

    using necessary packages

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using Fag_el_Gamous.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
