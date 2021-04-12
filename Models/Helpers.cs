7bd189ff50f16b2e6b7713bbf302ba8757d9c589
Devyn Smith <devyn.smith17@gmail.com>
2021-04-12 11:13:03 -0600

    test

--- Models/Helpers.cs
+++ Models/Helpers.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.IdentityModel.Protocols;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using System.Configuration;
+﻿using System.Configuration;
 
 namespace Fag_el_Gamous.Models
 {
